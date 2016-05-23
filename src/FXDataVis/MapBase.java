f4de262dd084d4915133499d6edffe1e1f767789
CS-Labs <cseely@unm.edu>
2016-05-23 00:21:09 -0600

    Adding final documentation.

--- src/FXDataVis/MapBase.java
+++ src/FXDataVis/MapBase.java
@@ -24,114 +24,133 @@ import javafx.stage.StageStyle;
  * 
  * @author Christian Seely
  * 
- * Controller class associated with the Map.fxml 
- * Initializes and displays the map. 
+ * Class to control the flow of the program and the creation of
+ * stages. 
  *
  */
-public class MapBase extends Application{
-	StackPane stackPane = new StackPane();
-    ScrollPane root;
-	//@FXML Canvas canvas;
-	SVGPaths SVG = new SVGPaths();
-	//Get the map image. 
-	BuildInteractiveRegions regionBuilder = new BuildInteractiveRegions(stackPane,this);
-	Image mapImage = (new Image(getClass().getResourceAsStream("/FXDataVis/map.png")));
-	ImageView map = new ImageView(mapImage);	
+public class MapBase extends Application
+{
+  StackPane stackPane = new StackPane();
+  ScrollPane root;
+  SVGPaths SVG = new SVGPaths();
+  // Get the map image.
+  BuildInteractiveRegions regionBuilder = new BuildInteractiveRegions(stackPane,
+      this);
+  Image mapImage = (new Image(
+      getClass().getResourceAsStream("/FXDataVis/map.png")));
+  ImageView map = new ImageView(mapImage);
 
-	/**
-	 * You cannot have constructors inside of controller classes as
-	 * the constructor is called prior to the FXML objects being 
-	 * initialized. To fix this there is a method called initialize this
-	 * gets called automatically if created and acts like a constructor
-	 * but is called after the FXML objects are instantiated. This method
-	 * initializes fields required for the map. 
-	 */
-	public void buildMap()
-	{
-		stackPane.getChildren().add(map);
-		map.toBack(); 		
-		regionBuilder.buildAfricaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-		regionBuilder.buildSouthAmericaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-		regionBuilder.buildMiddleAmericaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-		regionBuilder.buildCaliforniaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-		regionBuilder.buildHeartLandsPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-		regionBuilder.buildMiddleAmericaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-		regionBuilder.buildNorthernCrescentPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-		regionBuilder.buildNorthernPlanesPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildPNWAndMNTPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildSoutheastPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildSouthernPlanesDeltaStatesPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildMiddleEastPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildCentralAsiaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildSouthAsiaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildArcticAmericaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildRussiaCaucausPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildEuropePath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildEastAsiaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    regionBuilder.buildOceaniaPath(Color.CYAN, Color.CYAN, Color.rgb(0, 255, 255,0.5));
-	    root.setContent(stackPane);    
-	}
-	
-	
-	public void setBinds()
-	{
+  /**
+   * Method to add the map and then build the maps interactive regions. 
+   * For each specific region build call the meaning of the arguments
+   * are as follows:
+   * argument 1: Color of Regions boarder
+   * argument 2: Color of Regions boarder glow
+   * argument 3: Fill color of Region. 
+   */
+  public void buildMap()
+  {
+    stackPane.getChildren().add(map);
+    map.toBack(); //Regions must be displayed in front of map. 
+    regionBuilder.buildAfricaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildSouthAmericaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildMiddleAmericaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildCaliforniaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildHeartLandsPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildMiddleAmericaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildNorthernCrescentPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildNorthernPlanesPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildPNWAndMNTPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildSoutheastPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildSouthernPlanesDeltaStatesPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildMiddleEastPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildCentralAsiaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildSouthAsiaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildArcticAmericaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildRussiaCaucausPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildEuropePath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildEastAsiaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    regionBuilder.buildOceaniaPath(Color.CYAN, Color.CYAN,
+        Color.rgb(0, 255, 255, 0.5));
+    root.setContent(stackPane);
+  }
 
-//		System.out.println(canvas);
-//		canvas.widthProperty().bind(pane.widthProperty());
-//		canvas.heightProperty().bind(pane.heightProperty());
-	}
-	
-	
-	
-	/**
-	 * If the icon button is clicked then invoke the other stage
-	 * that displays the data visualization. This method is called via
-	 * the FXML onAction command for the icon button.
-	 * 
-	 *  So a problem with making a transparent scene is it takes
-	 *  away the ability to exit (fixed) and move the screen (TODO)
-	 */
-	public void invokeOtherStage()
-	{
-		try {
-			Stage newStage = new Stage();
-			//Make the stage transparent. 
-			newStage.initStyle(StageStyle.TRANSPARENT);		
-			
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXDataVis/DataVis.fxml"));			
-			Parent root = fxmlLoader.load();
-			FXStatisticalDisplaysController statController = (FXStatisticalDisplaysController) fxmlLoader.getController();
-			statController.setRegionBuilder(regionBuilder);
-			Scene scene = new Scene(root);
-			//Again needed for making the window
-			//transparent. 
-			scene.setFill(Color.TRANSPARENT);
-			newStage.setScene(scene);
-			newStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
+  /**
+   * Method to invoke a semi-transparent stage to appear over the map
+   * which to display the regional statistics of the region clicked.
+   * The stage is terminated once the user clicks the exit button.
+   * Note this technically acts as a separate java application, and 
+   * it is possible (and maybe better) to instead of creating a stage
+   * overlay creating a pane overlay on the original stage. 
+   */
+  public void invokeOtherStage()
+  {
+    try
+    {
+      Stage newStage = new Stage();
+      // Make the stage transparent.
+      newStage.initStyle(StageStyle.TRANSPARENT);
 
+      FXMLLoader fxmlLoader = new FXMLLoader(
+          getClass().getResource("/FXDataVis/DataVis.fxml"));
+      Parent root = fxmlLoader.load();
+      FXStatisticalDisplaysController statController = (FXStatisticalDisplaysController) fxmlLoader
+          .getController();
+      statController.setRegionBuilder(regionBuilder);
+      Scene scene = new Scene(root);
+      // Again needed for making the window
+      // transparent.
+      scene.setFill(Color.TRANSPARENT);
+      newStage.setScene(scene);
+      newStage.show();
+    } catch (Exception e)
+    {
+      e.printStackTrace();
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		root = new ScrollPane();
-		root.setPannable(true);
-		root.setPrefHeight(902);
-		root.setPrefWidth(1800);
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		buildMap();
-	}
-	
-	
-	public static void main(String[] args) {
-	launch(args);
-}
-
+  }
+  /**
+   * Create the initial Stage. 
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception
+  {
+    // TODO Auto-generated method stub
+    root = new ScrollPane();
+    root.setPannable(true);
+    root.setPrefHeight(902);
+    root.setPrefWidth(1800);
+    Scene scene = new Scene(root);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+    buildMap();
+  }
+  
+  /**
+   * 
+   * Fall through to invoke program if start is not called upon run. 
+   */
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
 
 }
